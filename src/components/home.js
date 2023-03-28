595991ee698586e76bb0c8c48cf8ef2ab6304197
Joseph Chan <josephchan36@gmail.com>
2023-03-27 23:29:10 -0700

    second  commit

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/index.css'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {Link} from "react-router-dom"
 
 function NavBar() {
   return (
